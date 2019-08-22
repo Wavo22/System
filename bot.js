c555fe083a1144afb054e6e5a938bb7a78a5ce3c
Wavo22 <54373191+Wavo22@users.noreply.github.com>
2019-08-22 17:12:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,33 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "?";
 
-client.login(process.env.BOT_TOKEN);
+var prefix = ('+');
 
-client.on('message', message =>{ 
 
-if(message.content === "Hello"){
-    message.channel.sendMessage('Hello ! How are you doing today? :smiley: ');
-    console.log('répond à H');
-}
+client.login('NjE0MDY5NDM3MjUwOTI4NjQw.XV6G8w.E2Cp99zUC565T2LsOTduaAdbQdA');
+
+client.on('guildMemberAdd', member =>{
+    member.guild.channels.get('605438284151128065').send(' **Hello** ' + member.user + ' **,** **Welcome to Kingdom** **,** **We are now** ' + member.guild.memberCount + ' **Members** **!** :heart: ');
+    console.log('+1')
 });
 
-/*Kick*/
-client.on('message', function (message) {
-    if (!message.guild) return
-    let args = message.content.trim().split(/ +/g)
- 
-    if (args[0].toLowerCase() === prefix + 'kick') {
-       if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send("You do not have the permission to use the kick command Al7mar ;(")
-       let member = message.mentions.members.first()
-       if (!member) return message.channel.send("**Please Mention an user**")
-       if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.owner.id) return message.channel.send("**You do not have the permission to kick this member**")
-       if (!member.kickable) return message.channel.send("**I do not have the permission to kick this member**")
-       member.kick()
-       message.channel.send('**' + member.user.username + '** Has been kicked from the server, Bye :o')
-    }
-})
-/*Ban*/
 client.on('message', function (message) {
     if (!message.guild) return
     let args = message.content.trim().split(/ +/g)
@@ -41,7 +24,22 @@ client.on('message', function (message) {
        message.guild.ban(member, {days: 7})
        message.channel.send('**' + member.user.username + '** Hes been banned from the server, Bye :o')
     }
-})
+});
+
+client.on('message', function (message) {
+    if (!message.guild) return
+    let args = message.content.trim().split(/ +/g)
+ 
+    if (args[0].toLowerCase() === prefix + 'kick') {
+       if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send("You do not have the permission to use the kick command Al7mar ;(")
+       let member = message.mentions.members.first()
+       if (!member) return message.channel.send("**Please Mention an user**")
+       if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.owner.id) return message.channel.send("**You do not have the permission to kick this member**")
+       if (!member.kickable) return message.channel.send("**I do not have the permission to kick this member**")
+       member.kick()
+       message.channel.send('**' + member.user.username + '** Has been kicked from the server, Bye :o')
+    }
+});
 
 client.on('message', function (message) {
     if (!message.guild) return
@@ -59,6 +57,7 @@ client.on('message', function (message) {
     if (args[0].toLowerCase() === prefix + "mute") {
         if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send("** You do not have the permission to use this commannd a l7mar**")
         let member = message.mentions.members.first()
+
         if (!member) return message.channel.send("** Member not found**")
         if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.ownerID) return message.channel.send("**You can not mute this member**")
         if (!member.manageable) return message.channel.send("**I can not mute this member**")
@@ -78,5 +77,29 @@ client.on('message', function (message) {
                 message.channel.send(member + ' Has been muted :o ')
             })
         }
+    }
+})
+
+
+
+   
+    
+
+
+
+Le jeu. 22 août 2019 à 15:13, Wavo <mr2mouj1@gmail.com> a écrit :
+/*Ban*/
+client.on('message', function (message) {
+    if (!message.guild) return
+    let args = message.content.trim().split(/ +/g)
+ 
+    if (args[0].toLocaleLowerCase() === prefix + 'ban') {
+       if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send("You do not have the permission to use the ban command al7mar ;(")
+       let member = message.mentions.members.first()
+       if (!member) return message.channel.send("Please Mention an user")
+       if (member.highestRole.calculatedPosition >= message.member.highestRole.calculatedPosition && message.author.id !== message.guild.owner.id) return message.channel.send("Vous ne pouvez pas bannir cet utilisateur :x:")
+       if (!member.bannable) return message.channel.send("I do not have the permission to ban this member")
+       message.guild.ban(member, {days: 7})
+       message.channel.send('**' + member.user.username + '** Hes been banned from the server, Bye :o')
     }
 })
